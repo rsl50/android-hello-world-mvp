ea33aa588a2d88974bc61a9b1b97152034ab1a57
Robson Lima <rsl.robson@gmail.com>
2022-03-29 23:06:06 -0300

    Connecting the View with the Presenter and Model using the Presenter.

--- app/src/main/java/com/android/helloworldmvp/view/MainActivity.java
+++ app/src/main/java/com/android/helloworldmvp/view/MainActivity.java
@@ -3,6 +3,8 @@ package com.android.helloworldmvp.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.helloworldmvp.R;
 import com.android.helloworldmvp.contract.IMainActivityContract;
@@ -13,6 +15,11 @@ public class MainActivity extends AppCompatActivity implements IMainActivityCont
     /** Holds the instance of the presenter */
     private IMainActivityContract.Presenter mPresenter;
 
+    /** Holds the instance of the {@link TextView} */
+    private TextView mTextView;
+
+    /** Holds the instance of the {@link Button} */
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +31,16 @@ public class MainActivity extends AppCompatActivity implements IMainActivityCont
 
     @Override
     public void initView() {
+        // Initialize the views intances.
+        mTextView = (TextView) findViewById(R.id.textView);
+        mButton = (Button) findViewById(R.id.button);
 
+        // Set button onClickListener using lambda and passing the event to the Presenter.
+        mButton.setOnClickListener(view -> mPresenter.onClick(view));
     }
 
     @Override
     public void setViewData(String data) {
-
+        mTextView.setText(data);
     }
 }
